7debf728f138d50cafc0baa8ba160f80dbbad585
DongerKai <koekchan@gmail.com>
2020-04-23 20:33:55 +0800

    tcp client

--- src/main/java/com/c503/tcp/client/core/client/ClientServer.java
+++ src/main/java/com/c503/tcp/client/core/client/ClientServer.java
@@ -68,7 +68,7 @@ public class ClientServer implements IServerService<ClientConnectVo> {
             log.info("============connect end :{} ======", connectEnd);
             log.info("===========连接建立时间：{}======", connectEnd-connectStart);
             size = clientConnect.getThreads()*clientConnect.getCycleTimes();
-//            send(clientConnect);
+            send(clientConnect);
         }catch (Exception e){
             log.error("连接有误",e);
         }
