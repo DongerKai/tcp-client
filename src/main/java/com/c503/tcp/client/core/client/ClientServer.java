64cefc0ea4fc08323b2e74690d0b24f6f420c747
DongerKai <koekchan@gmail.com>
2020-04-24 12:51:33 +0800

    tcp client

--- src/main/java/com/c503/tcp/client/core/client/ClientServer.java
+++ src/main/java/com/c503/tcp/client/core/client/ClientServer.java
@@ -135,14 +135,17 @@ public class ClientServer implements IServerService<ClientConnectVo> {
 
     @Override
     public void computeTime(ChannelHandlerContext ctx) {
-        Integer value = map.get(ctx.channel());
-        if (value == null)
-            return;
-        map.put(ctx.channel(), value-1);
-        if (value == 0 || value < 0){
-            long receiveEnd = System.currentTimeMillis();
-            log.info("==============最后一个回包接收时间：{}, map中value值:{} ,channel:{}, QPS:{}/s=============", receiveEnd, value, ctx.channel(), size*1000/(receiveEnd-sendBegin));
-        }
+        long receiveEnd = System.currentTimeMillis();
+        log.info("==============最后一个回包接收时间：{},channel:{}, QPS:{}/s=============", receiveEnd ctx.channel(), size*1000/(receiveEnd-sendBegin));
+
+//        Integer value = map.get(ctx.channel());
+//        if (value == null)
+//            return;
+//        map.put(ctx.channel(), value-1);
+//        if (value == 0 || value < 0){
+//            long receiveEnd = System.currentTimeMillis();
+//            log.info("==============最后一个回包接收时间：{}, map中value值:{} ,channel:{}, QPS:{}/s=============", receiveEnd, value, ctx.channel(), size*1000/(receiveEnd-sendBegin));
+//        }
 
     }
 
